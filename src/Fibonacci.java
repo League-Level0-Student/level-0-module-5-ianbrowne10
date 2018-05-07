ed42815e763f6128479ab416932c4b184b996726
ianbrowne10 <league@WorthyMini-5.attlocal.net>
2018-05-07 16:09:03 -0700

    Fibonacci, Goofy Names (IP)

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,13 +1,17 @@
+import javax.swing.JOptionPane;
 
 public class Fibonacci {
 public static void main(String[] args) {
 int num1 = 1;
 int num2 = 0;
 int sum = num1 + num2;	
-
-
+for (int i = 0; i < 10; i++) {
 	
-	
+num2 = num1;
+num1 = sum;	
+sum = num1 + num2;
+System.out.println(sum);
+}
 	
 	
 	
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -10,10 +10,10 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
@@ -23,9 +23,14 @@ public class GoofyNames {
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+        charAt%2==0{
+        	System.out.println(name.toLowerCase());
+        }
+        charAt%2==1{
+        	System.out.println(name.toUpperCase());
+        }
 			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+        // 6. ADD this String  (containing 1 char) to the goofyName String
 
 		
 		// 7. Use pop-up to show user their Goofy name
