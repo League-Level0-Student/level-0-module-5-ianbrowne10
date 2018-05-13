2e49eb04597f58ae4f0885be76b42bae972649fc
ianbrowne10 <league@WTS21.attlocal.net>
2018-05-13 12:56:39 -0700

    Circle Rings, Guessing Pi, For Loop Gauntlet (IP)

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,26 +9,33 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+Robot Jimmy = new Robot("batman");
 
        //  2. Set your robot’s position to x=150 and y=200
+Jimmy.setX(150);
+Jimmy.setY(200);      
+Jimmy.sparkle();
+// 3. Put the robot's pen down
+Jimmy.penDown();
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
+// 4. Set the robot’s speed to 10
+Jimmy.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
+for (int i = 0; i < 361; i++) {
 
 	        // 6. Move the robot 3 pixels
-
+	Jimmy.move(3);	
+	Jimmy.setRandomPenColor();
        //  7. Turn the robot 1 degree
-
+Jimmy.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
+if(i%20==0) {
+	for (int j = 0; j < 361; j++) {
+	
+	// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+Jimmy.move(1);
+Jimmy.setRandomPenColor();			// 10. Move the robot 1 pixel
+Jimmy.turn(1);
 				// 11. Turn the robot 1 degree
 }
-}
+}}}}
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -8,31 +8,49 @@ import javax.swing.JOptionPane;
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
 
+public static void main(String[] args) {
+	
+
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
+for (int i = 0; i < 10; i++) {
+	
 
+	String pi = "3.14";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
+//System.out.println(pi.charAt(0));
+//System.out.println(pi.charAt(1));
+//System.out.println(pi.charAt(2));
 
-		// 9. If you want to give the user more than one chance to guess,
+
+// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+for (int j = 0; j < 3; j++) {
+	System.out.print(pi.charAt(j));
+}
 				// 5. Ask the user for the NEXT digit of pi.
-
+String digit = JOptionPane.showInputDialog("What is the next digit?");
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
+int num = Integer.parseInt(digit);
+if(num == 4) {
+	JOptionPane.showMessageDialog(null, "Correct") ;
+} 
 
-					// 7. If they are correct, print out "correct".
-
-					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
-
+else  {
+	JOptionPane.showMessageDialog(null, "Incorrect! Try Again!");                 // 7. If they are correct, print out "correct".
 }
 
+break;					// 8. If they are not, print out "incorrect" and tell them
+					//    to start over. Use 'break;' to break out of the loop.
+}
+}
 
+}
